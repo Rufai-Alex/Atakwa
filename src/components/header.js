f7061546f8c2b5de9b0ec14668d526ea1bb98008
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-09-22 23:49:24 +0100

    Add navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,64 @@
 import React from 'react';
+import  kalma from "../image/banner-img-1.jpg";
 
 function HeaderTop() {
     return(
         <div>
-            <div>
-                <img src="../../image/kalma-text-img(1).png" alt="bismilla"/>
+            <nav  class="flex items-center bg-gray-800 p-3 flex-wrap">
+            <a href="#" class="p-2 mr-4 inline-flex items-center"> 
+            At-Taqwa
+            </a>
+            <button
+        class="text-white inline-flex p-3 hover:bg-gray-900 rounded lg:hidden ml-auto hover:text-white outline-none nav-toggler"
+        data-target="#navigation"
+      >
+        <i class="material-icons">menu</i>
+      </button>
+      <div
+        class="hidden top-navbar w-full lg:inline-flex lg:flex-grow lg:w-auto"
+        id="navigation"
+      >
+        <div
+          class="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto"
+        >
+          <a
+            href="#"
+            class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+          >
+            <span>Home</span>
+          </a>
+          <a
+            href="#"
+            class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+          >
+            <span>About</span>
+          </a>
+          <a
+            href="#"
+            class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+          >
+            <span>Services</span>
+          </a>
+          
+          <a
+            href="#"
+            class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+          >
+            <span>Products</span>
+          </a>
+          <a
+            href="#"
+            class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+          >
+            <span>Contact Us</span>
+          </a>
+        </div>
+      </div>
+            </nav>
+            <div className=" grid bg-black justify-items-center  ">
+                <img src={kalma} alt="bismilla"/>
             </div>
+
         </div>
     )
 
